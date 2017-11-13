1c0ee0b6b3cd9ec660b3888cca1a9fab6cd7d398
范叶 TroyFan <fan_ye@hotmail.com>
2017-11-13 18:25:59 +0800

    add echo example server-enable-heart

---
+++ examples/echo/server-enable-heart/server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/lygfanye/gotcp"
+	"github.com/lygfanye/gotcp/examples/echo"
+)
+
+func AfterConnect(c *gotcp.Conn) {
+	addr := c.GetRawConn().RemoteAddr()
+	c.PutExtraData(addr)
+	fmt.Println("OnConnect:", addr)
+}
+
+func AfterReceive(c *gotcp.Conn, p gotcp.Packet) {
+	echoPacket := p.(*echo.EchoPacket)
+	fmt.Printf("OnMessage:[%v] [%v]\n", echoPacket.GetLength(), string(echoPacket.GetBody()))
+	c.AsyncWritePacket(echo.NewEchoPacket(echoPacket.Pack(), true), time.Second)
+}
+
+func AfterClose(c *gotcp.Conn) {
+	fmt.Println("OnClose:", c.GetExtraData())
+}
+
+func main() {
+
+	sc := &gotcp.ServerConfig{
+		SendChanBuf:        6,
+		ReceiveChanBuf:     6,
+		AfterConnect:       AfterConnect,
+		AfterReceive:       AfterReceive,
+		AfterClose:         AfterClose,
+		EnableHeartBeating: true,
+		HeartBeatingThreshold : 20,
+		HeartBeatingPeriod : 10,
+	}
+
+	// creates a tcp listener
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", ":8980")
+	checkError(err)
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	srv := gotcp.NewServer(sc, &echo.EchoProtocol{})
+
+	// starts service
+	go srv.Start(listener)
+	fmt.Println("listening:", listener.Addr())
+
+	// catchs system signal
+	chSig := make(chan os.Signal)
+	signal.Notify(chSig, os.Interrupt)
+	<-chSig
+	// stops service
+	srv.Stop(listener)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
